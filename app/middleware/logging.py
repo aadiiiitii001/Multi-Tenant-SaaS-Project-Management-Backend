41abdeb325fb50d285012c3d3184495bdf30fdc9
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2026-03-13 18:21:09 +0530

    Update logging.py

--- app/middleware/logging.py
+++ app/middleware/logging.py
@@ -1,1 +1,17 @@
+from starlette.middleware.base import BaseHTTPMiddleware
+import time
 
+
+class LoggingMiddleware(BaseHTTPMiddleware):
+
+    async def dispatch(self, request, call_next):
+
+        start = time.time()
+
+        response = await call_next(request)
+
+        duration = time.time() - start
+
+        print(f"{request.url} completed in {duration}")
+
+        return response
