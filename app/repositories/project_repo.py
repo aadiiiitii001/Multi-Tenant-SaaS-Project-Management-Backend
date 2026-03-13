02337ec79b064b00b4ace463a91e7236a71e282a
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2026-03-13 18:28:18 +0530

    Update project_repo.py

--- app/repositories/project_repo.py
+++ app/repositories/project_repo.py
@@ -1,1 +1,59 @@
+from sqlalchemy.orm import Session
+from app.models.project import Project
 
+
+class ProjectRepository:
+
+    def create_project(self, db: Session, project_data: dict):
+        project = Project(**project_data)
+
+        db.add(project)
+        db.commit()
+        db.refresh(project)
+
+        return project
+
+
+    def get_project_by_id(self, db: Session, project_id: int):
+        return db.query(Project)\
+            .filter(Project.id == project_id)\
+            .first()
+
+
+    def get_projects_by_organization(self, db: Session, organization_id: int):
+        return db.query(Project)\
+            .filter(Project.organization_id == organization_id)\
+            .all()
+
+
+    def update_project(self, db: Session, project_id: int, update_data: dict):
+
+        project = db.query(Project)\
+            .filter(Project.id == project_id)\
+            .first()
+
+        if not project:
+            return None
+
+        for key, value in update_data.items():
+            setattr(project, key, value)
+
+        db.commit()
+        db.refresh(project)
+
+        return project
+
+
+    def delete_project(self, db: Session, project_id: int):
+
+        project = db.query(Project)\
+            .filter(Project.id == project_id)\
+            .first()
+
+        if not project:
+            return None
+
+        db.delete(project)
+        db.commit()
+
+        return project
