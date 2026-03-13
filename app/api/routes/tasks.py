43ddf824ff26871820c5553dda27708c758f6504
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2026-03-13 18:20:15 +0530

    Update tasks.py

--- app/api/routes/tasks.py
+++ app/api/routes/tasks.py
@@ -1,25 +1,30 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.core.database import SessionLocal
+from app.core.dependencies import get_db
+from app.services.task_service import TaskService
+from app.schemas.task_schema import TaskCreate
 from app.models.task import Task
 
-router = APIRouter()
+router = APIRouter(tags=["Tasks"])
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+task_service = TaskService()
 
-@router.get("/tasks")
-def get_tasks(page:int=1, db:Session=Depends(get_db)):
+
+@router.post("/")
+def create_task(data: TaskCreate, db: Session = Depends(get_db)):
+
+    task = task_service.create_task(db, data)
+
+    return task
+
+
+@router.get("/")
+def get_tasks(page: int = 1, db: Session = Depends(get_db)):
 
     limit = 20
-    offset = (page-1) * limit
+    offset = (page - 1) * limit
 
     tasks = db.query(Task)\
-        .order_by(Task.deadline)\
         .limit(limit)\
         .offset(offset)\
         .all()
