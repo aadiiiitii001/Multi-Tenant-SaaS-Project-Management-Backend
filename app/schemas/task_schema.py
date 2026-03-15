0d6fea4e32f58b4c260d2b03ddb16f7d95b771d7
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2026-03-15 17:51:53 +0530

    Update task_schema.py

--- app/schemas/task_schema.py
+++ app/schemas/task_schema.py
@@ -1,1 +1,18 @@
+from pydantic import BaseModel
+from datetime import datetime
 
+
+class TaskCreate(BaseModel):
+    title: str
+    status: str
+    deadline: datetime
+    project_id: int
+
+
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+    status: str
+
+    class Config:
+        orm_mode = True
