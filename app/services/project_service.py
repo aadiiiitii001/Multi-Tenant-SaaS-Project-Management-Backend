be58bd0114b3a4a5986e6c87af20ebd392f3fae4
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2026-03-15 17:49:29 +0530

    Update project_service.py

--- app/services/project_service.py
+++ app/services/project_service.py
@@ -1,1 +1,15 @@
+from sqlalchemy.orm import Session
+from app.models.project import Project
 
+
+class ProjectService:
+
+    def create_project(self, db: Session, data):
+
+        project = Project(**data.dict())
+
+        db.add(project)
+        db.commit()
+        db.refresh(project)
+
+        return project
