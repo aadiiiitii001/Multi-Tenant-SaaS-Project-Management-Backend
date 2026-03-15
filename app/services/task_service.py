e6a6d037570dd66f9ac284e54cd753e4b7581528
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2026-03-15 17:49:54 +0530

    Update task_service.py

--- app/services/task_service.py
+++ app/services/task_service.py
@@ -1,1 +1,15 @@
+from sqlalchemy.orm import Session
+from app.models.task import Task
 
+
+class TaskService:
+
+    def create_task(self, db: Session, data):
+
+        task = Task(**data.dict())
+
+        db.add(task)
+        db.commit()
+        db.refresh(task)
+
+        return task
