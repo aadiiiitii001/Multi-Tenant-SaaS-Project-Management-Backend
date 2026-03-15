03ab95103a40a11e57cffa0b9b028bc69cad2f5d
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2026-03-15 17:51:19 +0530

    Update project_schema.py

--- app/schemas/project_schema.py
+++ app/schemas/project_schema.py
@@ -1,1 +1,14 @@
+from pydantic import BaseModel
 
+
+class ProjectCreate(BaseModel):
+    name: str
+    organization_id: int
+
+
+class ProjectResponse(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        orm_mode = True
